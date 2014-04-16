4d6f375c852abe534f4ffed7b16e57550e84cd9d
Gage Bachik <gbachik@gmail.com>
2014-04-16 10:36:40 -0700

    updates

--- Assets/Scripts/Score.js
+++ Assets/Scripts/Score.js
@@ -5,3 +5,11 @@ static var playerScore : int = 0;
 static function score() {
 	playerScore += 1;
 }
+
+static function resetScore() {
+	playerScore = 0;
+}
+
+static function getScore() {
+	return playerScore;
+}
--- Assets/Scripts/icicleController.js
+++ Assets/Scripts/icicleController.js
@@ -1,11 +1,9 @@
 ﻿#pragma strict
 
+static var finalScore : int = 0;
 function Start () {
-
-}
-
-function Update () {
-
+	var myRigidBody : Rigidbody2D = GetComponent(Rigidbody2D);
+		myRigidBody.gravityScale = Random.Range(0.01,0.5);
 }
 
 function OnTriggerExit2D (coll : Collider2D) {
@@ -14,6 +12,12 @@ function OnTriggerExit2D (coll : Collider2D) {
 			Destroy(this.gameObject);
 		} else if (coll.gameObject.tag == 'limb'){
 			Destroy(coll.gameObject);
+		} else if (coll.gameObject.tag == 'vital'){
+			finalScore = Score.getScore();
+			Debug.Log(finalScore);
+			Destroy(coll.gameObject);
+			Application.LoadLevel('game');
+			Score.resetScore();
 		}
 		
 }
